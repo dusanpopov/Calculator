5f0d509a0e0ac622eb6606b45f7a28f96cff8f5b
dusanpopov <aristocut90@gmail.com>
2019-01-20 22:35:42 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+const btns = document.querySelectorAll(".calculator__buttons-number");
+const display = document.querySelector(".display");
+const equalsBtn = document.querySelector(".equals-btn");
+const clearBtn = document.querySelector(".clear-btn");
+
+
+btns.forEach(btn => {
+
+    btn.addEventListener("click", () => {
+
+        let number = btn.getAttribute('data-num');
+        display.value += number;
+
+    })
+});
+
+
+equalsBtn.addEventListener("click", () => {
+
+    if (display.value === "") {
+
+        display.value = `Please enter a value`
+
+    } else {
+
+        let value = eval(display.value)
+        display.value = value;
+
+    }
+});
+
+
+clearBtn.addEventListener("click", () => {
+
+    display.value = "";
+
+});
